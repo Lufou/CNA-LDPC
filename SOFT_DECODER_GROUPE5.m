de56b94af776c484d60181875745df7a293a6776
SalmaBahar <91837917+SalmaBahar@users.noreply.github.com>
2023-10-29 22:41:08 +0100

    Update SOFT_DECODER_GROUPE5.m

--- SOFT_DECODER_GROUPE5.m
+++ SOFT_DECODER_GROUPE5.m
@@ -1,4 +1,40 @@
 function result = SOFT_DECODER_GROUPE5(c, H, p, MAX_ITER)
+  
+   function isBinaryVector = isBinaryVector(vector)
+        % Vérifie si le vecteur ne contient que des 0 et des 1
+        uniqueValues = unique(vector);
+        isBinaryVector = isequal(uniqueValues, [0; 1]);
+   end
+   
+   function isProbabilityVector = isProbabilityVector(vector)
+        % Vérifie si le vecteur contient des probabilités entre 0 et 1
+        isProbabilityVector = all(vector >= 0 & vector <= 1);
+   end
+   
+   function isBinaryMatrix = isBinaryMatrix(matrix)
+        % Vérifie si la matrice ne contient que des valeurs binaires (0 et 1)
+        uniqueValues = unique(matrix);
+        isBinaryMatrix = isequal(uniqueValues, [0; 1]);
+   end
+
+    [nb_lines_c, nb_col_c] = size(c);
+    [nb_lines_p, nb_col_p] = size(p);
+    [nb_lines_H, nb_col_H] = size(H);
+    
+    % Vérifier les paramètres c, p et H
+    if nb_lines_c ~= nb_lines_H || nb_col_c > 1 || ~isBinaryVector(c)
+        disp("c doit être un vecteur colonne binaire de dimension [N, 1] avec N égal au nombre de colonnes de H.");
+        return
+    elseif nb_lines_p ~= nb_lines_c || nb_col_p > 1 || ~isProbabilityVector(p)
+        disp("p doit être un vecteur colonne de probabilités entre 0 et 1 de dimension [N, 1] comme le vecteur c");
+        return
+    elseif ~isBinaryMatrix(H)
+        disp("H doit être une matrice de parité binaire de dimension [M, N].");
+        return
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   
    parity = 0;
    iter = 0;
    [nb_lines_H, nb_col_H] = size(H);
